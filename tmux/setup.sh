35da68dbe058a0c0cc12d935f280a03b42b9c236
mingchaoyan <mingchaoyan@gmail.com>
2016-12-29 15:00:10 +0800

    tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,2 +1,5 @@
 cp -f $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+cp -apf $HOME/.dotfiles/tmux/tmux-powerline $HOME/.tmux-powerline
+cp -f $HOME/.dotfiles/tmux/tmux-powerlinerc $HOME/.tmux-powerlinerc
+
 
