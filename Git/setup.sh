cd3739db0ada6f8bf60e881f8c2dbe301f0f38ad
mingchaoyan <mingchaoyan@gmail.com>
2017-01-03 14:19:14 +0800

    update

--- Git/setup.sh
+++ Git/setup.sh
@@ -1,2 +1,12 @@
 cp -f $HOME/.dotfiles/Git/gitconfig $HOME/.gitconfig
 cp -f $HOME/.dotfiles/Git/gitignore $HOME/.gitignore_global
+
+platform=$(uname)
+
+if [[ $platform == *Darwin* ]]
+then
+  cat $HOME/.dotfiles/Git/git.conf >> ~/.zshrc
+else
+  cat $HOME/.dotfiles/Git/git.conf >> ~/.bashrc
+fi
+
---
+++ zsh/README.md
@@ -0,0 +1,3 @@
+# Zsh
+
+## on-my-zsh
